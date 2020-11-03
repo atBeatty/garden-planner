669e942b77463291d7a48f18cc5cfe0144de8f78
atBeatty <adam.t.beatt@gmail.com>
2020-11-03 15:18:31 -0800

    login flow users created succesfully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_or_create_by(user_params)
+        binding.pry
+
+        redirect_to users_path
+
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
    has_many :plants
    has_many :gardens, through: :plants
+
+   validates :email, uniqueness: true
    
    
 end
